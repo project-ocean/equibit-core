b92a723db93b60acc276b4ad6ef42e0c5541aff0
Sergei Sachkov <sergei@equibitgroup.com>
2017-07-05 13:51:56 -0400

    EQB - Added build scripts

---
+++ edc/linux-ubuntu/build.scripts/set.environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+script_name=$(realpath "$BASH_SOURCE")
+script_folder=$(dirname "$script_name")
+source_folder=$(realpath "$script_folder/../../..")
+thridparty_folder=$(realpath "$source_folder/../thirdparty")
+berkley_folder="$thridparty_folder/db4"
+
+echo "*** Set environment variables ***"
+echo "script_name      " = "$script_name"
+echo "script_folder    " = "$script_folder"
+echo "source_folder    " = "$source_folder"
+echo "thridparty_folder" = "$thridparty_folder"
+echo "berkley_folder   " = "$berkley_folder"
+echo ""
