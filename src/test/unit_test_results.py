6e7fdc6cb94f3d0207530c8dc35b0ebf48d757cc
Alex Fadeev <ealefad@gmail.com>
2018-07-19 17:37:13 -0400

    #24 Update (unit_test_results.py)

--- src/test/unit_test_results.py
+++ src/test/unit_test_results.py
@@ -3,6 +3,7 @@
 
 import sys
 import re
+import datetime
 
 with open(sys.argv[1], 'r', newline='\n') as f:
     contents = f.read().splitlines()
@@ -12,16 +13,20 @@ tSuite = {}
 tCase = {}
 tError = []
 log_header = ""
+log_file = ""
+tests_n = 0
 
+# Parse log file into the hierarchical structure
 for ln in contents:
-    tests_number = re.match(r'.*Running \d+ test.*', ln)
-    module = re.match(r'.*(Entering test module)\s\"(.+)\"', ln)    # Entering test module "Bitcoin Test Suite"
-    suite = re.match(r'.*(Entering test suite)\s\"(.+)\"', ln)      # test/mempool_tests.cpp(15): Entering test suite "mempool_tests"
-    testcase = re.match(r'.*(Entering test case)\s\"(.+)\"', ln)    # test/mempool_tests.cpp(433): Entering test case "MempoolSizeLimitTest"
-    testerror = re.match(r'.*(error: in)\s\"(.+)\":\s(.*)', ln)     # test/mempool_tests.cpp(577): error: in "mempool_tests/MempoolSizeLimitTest": check pool.GetMinFee(1).GetFeePerK() == 1000 has failed
+    tests_number = re.match(r'.*Running (\d+) test.*', ln)
+    module = re.match(r'.*(Entering test module)\s\"(.+)\"', ln)
+    suite = re.match(r'.*(Entering test suite)\s\"(.+)\"', ln)
+    testcase = re.match(r'.*(Entering test case)\s\"(.+)\"', ln)
+    testerror = re.match(r'.*(error: in)\s\"(.+)\":\s(.*)', ln)
 
     if tests_number is not None:
         log_header += ln + "\n"
+        tests_n = int(tests_number.group(1))
     if suite is not None:
         tCase = {}
         tests_result[suite.group(2)] = tCase
@@ -31,20 +36,45 @@ for ln in contents:
     if testerror is not None:
         tError.append(testerror.group(3))
 
-#print(tests_result)
+suites_with_errors = 0
+tCases_with_errors = 0
+total_errors = 0
+
+now = datetime.datetime.now()
+log_header += str(now.strftime("%Y-%m-%d %H:%M:%S")) + "\n"
+print(log_header)
+
+# Traverse tests_result{} and print summary to the screen and complete log to the file
+for sui in sorted(tests_result.keys()):
+    total_errors_suite = 0
+    log_file += "Test Suite: %s\n\n" % sui
+
+    for cs in sorted(tests_result[sui].keys()):
+        numErrors = len(tests_result[sui][cs])
+        if numErrors > -1:  # -1 prints all test cases, 0 prints only those with errors
+            log_file += "Test Case: {0:50} Errors: {1:5}\n".format(cs, numErrors)
+            total_errors_suite += numErrors
+            total_errors += numErrors
+        if numErrors > 0:
+            tCases_with_errors += 1
+
+    if total_errors_suite > 0:
+        suites_with_errors += 1
+
+    log_file += " " * 63 + "TOTAL: {:5}\n".format(total_errors_suite)
+    log_file += "-----------------------------------------------------------------------------\n"
+    print("Test Suite: {0:40} TOTAL ERRORS: {1:5}".format(sui, total_errors_suite))
+
+print("-----------------------------------------------------------------------------")
+print("Suites with errors:     {:5}".format(suites_with_errors))
+print("Test Cases with errors: {:5}".format(tCases_with_errors))
+print("Errors total:           {:5}".format(total_errors))
+print("Executed test cases:    {:5}".format(tests_n))
+
 with open("unit_test_results.log", "w") as file_log:
     print(log_header, file=file_log)
-    print(log_header)
-    for sui in sorted(tests_result.keys()):
-        total_errors_suite = 0
-        print("Test Suite: %s\n" %sui, file=file_log)
-        for cs in sorted(tests_result[sui].keys()):
-            numErrors = len(tests_result[sui][cs])
-            if numErrors > -1:  # print all test cases, including with no errors
-                print("Test Case: {0:50} Errors: {1:5}".format(cs, numErrors), file=file_log)
-                total_errors_suite += numErrors
-        print("TOTAL ERRORS: {:6}".format(total_errors_suite), file=file_log)
-        print("-----------------------------------------------------------------------------", file=file_log)
-
-        print("Test Suite: {0:40} TOTAL ERRORS: {1:5}".format(sui, total_errors_suite))
-
+    print("Suites with errors:     {:5}".format(suites_with_errors), file=file_log)
+    print("Test Cases with errors: {:5}".format(tCases_with_errors), file=file_log)
+    print("Errors total:           {:5}".format(total_errors), file=file_log)
+    print("-----------------------------------------------------------------------------", file=file_log)
+    print(log_file, file=file_log)
