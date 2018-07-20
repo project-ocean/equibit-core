20d30822abed25234632a835d4cfc8f221059095
Sergei Sachkov <sergei@equibitgroup.com>
2018-07-20 14:09:16 -0400

    EQB - Cleaned up Issuer RPC commands (parameters, descriptions, implementation standards)

--- src/edc/rpc/edcissuer.cpp
+++ src/edc/rpc/edcissuer.cpp
@@ -22,29 +22,33 @@ namespace
 std::string IssuerFromValue(const UniValue& value)
 {
     std::string issuer = value.get_str();
+
     if (issuer == "*")
         throw JSONRPCError(RPC_WALLET_INVALID_ISSUER_NAME, "Invalid issuer name");
+
     return issuer;
 }
 
 UniValue getNewIssuer(const JSONRPCRequest& request)
 {
-    if (!EnsureWalletIsAvailable(request.fHelp))
-        return NullUniValue;
+    if (!EnsureWalletIsAvailable(request.fHelp)) return NullUniValue;
 
     if (request.fHelp || request.params.size() < 4)
         throw std::runtime_error(
-            "getnewissuer \"name\" \"location\" \"phone-number\" \"e-mail address\"\n"
-            "\nCreates a new Issuer.\n"
-            "\nArguments:\n"
-            "1. \"Name\"            (string,required) The name of the Issuer.\n"
-            "2. \"Location\"        (string,required) The geographic address of the Issuer.\n"
-            "3. \"Phone number\"    (string,required) The phone number of the Issuer.\n"
-            "4. \"E-mail address\"  (string,required) The e-mail address of the Issuer.\n"
-            "\nResult:\n"
-            "The address associated with the Issuer.\n"
-            + HelpExampleCli("getnewissuer", "\"Equibit Issuer\" \"100 University Ave, Toronto\" \"416 233-4753\" \"equibit-issuer.com\"")
-            + HelpExampleRpc("getnewissuer", "\"Equibit Issuer\" \"100 University Ave, Toronto\" \"416 233-4753\" \"equibit-issuer.com\"")
+            R"__(getnewissuer "name" "location" "phone_number" "email"
+
+ Creates a new Issuer.
+
+ Arguments:
+ 1. name            (string, required) The name of the Issuer.
+ 2. location        (string, required) The geographic address of the Issuer.
+ 3. phone_number    (string, required) The phone number of the Issuer.
+ 4. email           (string, required) The e-mail address of the Issuer.
+
+ Result:
+ The address associated with the Issuer.)__"
+            + HelpExampleCli("getnewissuer", R"__("Equibit Issuer"  "100 University Ave, Toronto"  "416 233-4753"  "equibit-issuer.com")__")
+            + HelpExampleRpc("getnewissuer", R"__("Equibit Issuer", "100 University Ave, Toronto", "416 233-4753", "equibit-issuer.com")__")
         );
 
     std::string name = request.params[0].get_str();
@@ -52,12 +56,11 @@ UniValue getNewIssuer(const JSONRPCRequest& request)
     std::string phoneNumber = request.params[2].get_str();
     std::string emailAddr = request.params[3].get_str();
 
-    CIssuer	issuer(location, phoneNumber, emailAddr);
+    CIssuer issuer(location, phoneNumber, emailAddr);
 
     LOCK2(cs_main, pwalletMain->cs_wallet);
 
-    if (!pwalletMain->IsLocked())
-        pwalletMain->TopUpKeyPool();
+    if (!pwalletMain->IsLocked()) pwalletMain->TopUpKeyPool();
 
     if (!pwalletMain->GetKeyFromPool(issuer.pubKey_))
         throw JSONRPCError(RPC_WALLET_KEYPOOL_RAN_OUT, "Error: Keypool ran out, please call keypoolrefill first");
@@ -81,58 +84,57 @@ UniValue listIssuers(const JSONRPCRequest& request)
 {
     if (request.fHelp)
         throw std::runtime_error(
-            "getissuers\n"
-            "\nLists all known Issuers.\n"
-            "\nResult:\n"
-            "[                                 (json object)\n"
-            "  {                               (json object)\n"
-            "    \"name\": name,               (string) name of the issuer\n"
-            "    \"address\": address,         (string) equibit address of the issuer\n"
-            "    \"pubKey\": pubkey,           (string) public key of the issuer\n"
-            "    \"location\": location,       (string) geographic address of the issuer\n"
-            "    \"e-mail\": e-mail-address,   (string) e-mail address of the issuer\n"
-            "    \"phone\": phone-number,      (string) phone number of the issuer\n"
-            "  }, ...\n"
-            "]\n"
+            R"__(getissuers
+
+ Lists all known Issuers.
+
+ Result: (json object)
+ [
+   {
+     "name":         name,           (string) name of the issuer
+     "address":      address,        (string) Equibit address of the issuer
+     "public_key":   public_key,     (string) public key of the issuer
+     "location":     location,       (string) geographic address of the issuer
+     "email":        email,          (string) e-mail address of the issuer
+     "phone_number": phone_number,   (string) phone number of the issuer
+   }, ...
+ ])__"
             + HelpExampleCli("getissuers", "")
             + HelpExampleRpc("getissuers", "")
         );
 
     CWalletDB walletdb(pwalletMain->strWalletFile);
 
-    std::vector<std::pair<std::string, CIssuer>>	issuers;
+    std::vector<std::pair<std::string, CIssuer>> issuers;
+
     walletdb.ListIssuers(issuers);
 
-    std::vector<std::pair<std::string, CIssuer>>::iterator i = issuers.begin();
-    std::vector<std::pair<std::string, CIssuer>>::iterator e = issuers.end();
-
     std::stringstream out;
+
     out << "[\n";
 
     bool first = true;
-    while (i != e)
-    {
-        const std::string & name = i->first;
-        const CIssuer & issuer = i->second;
 
-        if (!first)
-            out << ",\n";
-        else
-            first = false;
+    for (auto &i : issuers)
+    {
+        const std::string & name = i.first;
+        const CIssuer & issuer = i.second;
+
+        if (!first) out << ",\n";
+        else        first = false;
 
         CKeyID keyID = issuer.pubKey_.GetID();
+
         CBitcoinAddress address(keyID);
 
-        out << "  {"
+        out << "{"
             << "\"name\": \"" << name << "\""
             << ", \"address\":\"" << address.ToString() << "\""
-            << ", \"pubKey\":\"" << HexStr(issuer.pubKey_) << "\""
+            << ", \"public_key\":\"" << HexStr(issuer.pubKey_) << "\""
             << ", \"location\":\"" << issuer.location_ << "\""
             << ", \"email\":\"" << issuer.emailAddress_ << "\""
             << ", \"phone_number\":\"" << issuer.phoneNumber_ << "\""
             << "}";
-
-        ++i;
     }
     out << "\n]";
 
@@ -143,44 +145,39 @@ UniValue listIssuers(const JSONRPCRequest& request)
 
 UniValue authorizeEquibit(const JSONRPCRequest& request)
 {
-    if (!EnsureWalletIsAvailable(request.fHelp))
-        return NullUniValue;
+    if (!EnsureWalletIsAvailable(request.fHelp)) return NullUniValue;
 
     if (request.fHelp || request.params.size() < 3 || request.params.size() > 5)
         throw std::runtime_error(
-            "authorizeequibit \"issuer\" amount wot-min-lvl ( \"comment\" subtractfeefromamount )\n"
-            "\nAuthorizes (or labels) an eqibit.\n"
-            "\nArguments:\n"
-            "1. \"Issuer\"          (string,required) The issuer that will be authorizing the Equibit.\n"
-            "2. amount              (numeric,required) The amount of coins to authorize.\n"
-            "3. wot-min-lvl         (numeric,required) The minimum WoT level used when TXN is moved.\n"
-            "4. \"comment\"         (string, optional) A comment used to store what the transaction is for. \n"
-            "                             This is not part of the transaction, just kept in your wallet.\n"
-            "                             transaction, just kept in your wallet.\n"
-            "5. subtractfeefromamount  (boolean, optional, default=false) The fee will be deducted from the amount being sent.\n"
-            "                             The recipient will receive less equibits than you enter in the amount field.\n"
+            R"__(authorizeequibit "issuer" amount wot_min_level ("comment") (subtract_fee_from_amount)
 
+ Authorizes or labels eqibits.
 
-
-            "\nResult:\n"
-            "\"transactionid\"        (string) The id of the generated transaction.\n"
-
-            + HelpExampleCli("authorizeequibit", "\"ABC Comp\" 1000 2")
-            + HelpExampleRpc("authorizeequibit", "\"ABC Comp\", 1000, 2")
+ Arguments:
+ 1. issuer                    (string,  required) The issuer that will be authorizing equibits.
+ 2. amount                    (numeric, required) The amount of equibits to authorize.
+ 3. wot_min_level             (numeric, required) The minimum WoT level used when TXN is moved.
+ 4. comment                   (string,  optional) A comment used to store what the transaction is for.
+                                                  This is not part of the transaction, just kept in your wallet.
+ 5. subtract_fee_from_amount  (boolean, optional, default = false) The fee will be deducted from the amount being sent.
+                                                  The recipient will receive less equibits than you enter in the amount field.
+ Result:
+ transactionid                (string) The id of the generated transaction.)__"
+            + HelpExampleCli("authorizeequibit", R"__("ABC Comp"  1000  2  "comment"  true)__")
+            + HelpExampleRpc("authorizeequibit", R"__("ABC Comp", 1000, 2, "comment", true)__")
         );
 
     LOCK2(cs_main, pwalletMain->cs_wallet);
 
     std::string iName = IssuerFromValue(request.params[0]);
-    CAmount 	amount = AmountFromValue(request.params[1]);
-    unsigned	WoTlvl = request.params[2].get_int();
+    CAmount amount = AmountFromValue(request.params[1]);
+    unsigned WoTlvl = request.params[2].get_int();
 
     if (amount <= 0)
         throw JSONRPCError(RPC_TYPE_ERROR, "Invalid amount for authorization");
 
     CWalletTx wtxNew;
 
-    // Wallet comments
     if (request.params.size() > 3 && !request.params[3].isNull() && !request.params[3].get_str().empty())
         wtxNew.mapValue["comment"] = request.params[3].get_str();
 
@@ -200,7 +197,6 @@ UniValue authorizeEquibit(const JSONRPCRequest& request)
 
     CWalletDB walletdb(pwalletMain->strWalletFile);
 
-    // Get issuer address
     CIssuer issuer;
     if (!walletdb.ReadIssuer(iName, issuer))
         throw JSONRPCError(RPC_WALLET_INVALID_ISSUER_NAME, "Invalid issuer name");
@@ -208,7 +204,6 @@ UniValue authorizeEquibit(const JSONRPCRequest& request)
     CKeyID id = issuer.pubKey_.GetID();
     CTxDestination address = CBitcoinAddress(id).Get();
 
-    // Parse Equibit address
     CScript scriptPubKey = GetScriptForDestination(address);
 
     // Create and send the transaction
@@ -221,11 +216,11 @@ UniValue authorizeEquibit(const JSONRPCRequest& request)
 
     vecSend.push_back(recipient);
 
-    if (!pwalletMain->CreateAuthorizingTransaction(issuer, WoTlvl,
-                                                   vecSend, wtxNew, reservekey, nFeeRequired, nChangePosRet, strError))
+    if (!pwalletMain->CreateAuthorizingTransaction(issuer, WoTlvl, vecSend, wtxNew, reservekey, nFeeRequired, nChangePosRet, strError))
     {
         if (amount > pwalletMain->GetBalance())
             strError = strprintf("Error: This transaction requires a transaction fee of at least %s because of its amount, complexity, or use of recently received funds!", FormatMoney(nFeeRequired));
+
         throw JSONRPCError(RPC_WALLET_ERROR, strError);
     }
 
@@ -241,38 +236,35 @@ UniValue authorizeEquibit(const JSONRPCRequest& request)
 
 UniValue blankEquibit(const JSONRPCRequest& request)
 {
-    if (!EnsureWalletIsAvailable(request.fHelp))
-        return NullUniValue;
+    if (!EnsureWalletIsAvailable(request.fHelp)) return NullUniValue;
 
     if (request.fHelp || request.params.size() < 2 || request.params.size() > 5)
         throw std::runtime_error(
-            "blankequibit \"issuer\" amount ( \"comment\" subtractfeefromamt feefromblank )\n"
-            "\nAuthorizes (or labels) an eqibit.\n"
-            "\nArguments:\n"
-            "1. \"Issuer\"          (string,required) The issuer that will be authorizing the Equibit.\n"
-            "2. amount              (numeric,required) The amount of coins to authorize.\n"
-            "3. \"comment\"         (string, optional) A comment used to store what the transaction is for. \n"
-            "                             This is not part of the transaction, just kept in your wallet.\n"
-            "                             transaction, just kept in your wallet.\n"
-            "4. subtractfeefromamt  (boolean, optional, default=false) The fee will be deducted from the amount being sent.\n"
-            "5. feefromblank        (boolean, optional, default=true) The fee is paid from blank equitbits.\n"
-            "\nResult:\n"
-            "\"transactionid\"      (string) The id of the generated transaction.\n"
+            R"__(blankequibit "issuer" amount ("comment") (subtract_fee_from_amount) (fee_from_blank)
+ Authorizes or labels equibits
+ Arguments:
+ 1. issuer                    (string,  required) The issuer that will be authorizing equibits.
+ 2. amount                    (numeric, required) The amount of equibits to authorize.
+ 3. comment                   (string,  optional) A comment used to store what the transaction is for.
+                                                  This is not part of the transaction, just kept in your wallet.\n"
+ 4. subtract_fee_from_amount  (boolean, optional, default = false) The fee will be deducted from the amount being sent.\n"
+ 5. fee_from_blank            (boolean, optional, default = true)  The fee is paid from blank equibits.\n"
 
-            + HelpExampleCli("blankequibit", "\"ABC Comp\" 1000")
-            + HelpExampleRpc("blankequibit", "\"ABC Comp\", 1000")
+ Result:
+ transactionid                (string) The id of the generated transaction.)__"
+            + HelpExampleCli("blankequibit", R"__("ABC Comp"  1000  "comment"  true  false)__")
+            + HelpExampleRpc("blankequibit", R"__("ABC Comp", 1000, "comment", true, false)__")
         );
     LOCK2(cs_main, pwalletMain->cs_wallet);
 
     std::string iName = IssuerFromValue(request.params[0]);
-    CAmount 	amount = AmountFromValue(request.params[1]);
+    CAmount amount = AmountFromValue(request.params[1]);
 
     if (amount <= 0)
         throw JSONRPCError(RPC_TYPE_ERROR, "Invalid amount for authorization");
 
     CWalletTx wtxNew;
 
-    // Wallet comments
     if (request.params.size() > 2 && !request.params[2].isNull() && !request.params[2].get_str().empty())
         wtxNew.mapValue["comment"] = request.params[2].get_str();
 
@@ -296,7 +288,6 @@ UniValue blankEquibit(const JSONRPCRequest& request)
 
     CWalletDB walletdb(pwalletMain->strWalletFile);
 
-    // Get issuer address
     CIssuer issuer;
     if (!walletdb.ReadIssuer(iName, issuer))
         throw JSONRPCError(RPC_WALLET_INVALID_ISSUER_NAME, "Invalid issuer name");
@@ -304,10 +295,8 @@ UniValue blankEquibit(const JSONRPCRequest& request)
     CKeyID id = issuer.pubKey_.GetID();
     CTxDestination address = CBitcoinAddress(id).Get();
 
-    // Parse Equibit address
     CScript scriptPubKey = GetScriptForDestination(address);
 
-    // Create and send the transaction
     CReserveKey reservekey(pwalletMain);
     CAmount nFeeRequired;
     std::string strError;
@@ -317,11 +306,11 @@ UniValue blankEquibit(const JSONRPCRequest& request)
 
     vecSend.push_back(recipient);
 
-    if (!pwalletMain->CreateBlankingTransaction(issuer, vecSend, wtxNew, reservekey,
-                                                feeFromBlank, nFeeRequired, nChangePosRet, strError))
+    if (!pwalletMain->CreateBlankingTransaction(issuer, vecSend, wtxNew, reservekey, feeFromBlank, nFeeRequired, nChangePosRet, strError))
     {
         if (amount > pwalletMain->GetBalance())
             strError = strprintf("Error: This transaction requires a transaction fee of at least %s because of its amount, complexity, or use of recently received funds!", FormatMoney(nFeeRequired));
+
         throw JSONRPCError(RPC_WALLET_ERROR, strError);
     }
 
@@ -336,19 +325,17 @@ UniValue blankEquibit(const JSONRPCRequest& request)
 }
 
 const CRPCCommand commands[] =
-{ // category   name                actor (function)   okSafeMode
-  // ---------- ------------------- ------------------ -------------
-
-    { "equibit", "getnewissuer",		 &getNewIssuer,      true },
-    { "equibit", "getissuers",		 &listIssuers,       true },
-    { "equibit", "authorizeequibit",  &authorizeEquibit,  true },
-    { "equibit", "blankequibit",      &blankEquibit,      true },
+{
+    { "equibit", "getnewissuer",     &getNewIssuer,      true, { "name", "location", "phone_number", "email" } },
+    { "equibit", "getissuers",       &listIssuers,       true, {} },
+    { "equibit", "authorizeequibit", &authorizeEquibit,  true, { "issuer", "amount", "wot_min_level", "comment", "subtract_fee_from_amount" } },
+    { "equibit", "blankequibit",     &blankEquibit,      true, { "issuer", "amount", "comment", "subtract_fee_from_amount", "fee_from_blank" } },
 };
 
 }
 
-void RegisterIssuerRPCCommands(CRPCTable & tableRPC)
+void RegisterIssuerRPCCommands(CRPCTable& tableRPC)
 {
-    for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
+    for (size_t vcidx = 0; vcidx < std::extent<decltype(commands)>::value; vcidx++)
         tableRPC.appendCommand(commands[vcidx].name, &commands[vcidx]);
 }
