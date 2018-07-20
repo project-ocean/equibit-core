d6d739e63aa66b6c481b85042f98bbb96f66f4be
Sergei Sachkov <sergei@equibitgroup.com>
2018-07-20 13:39:39 -0400

    EQB - Added an Equibit specific RPC error code

--- src/rpc/protocol.h
+++ src/rpc/protocol.h
@@ -75,6 +75,9 @@ enum RPCErrorCode
     RPC_WALLET_WRONG_ENC_STATE      = -15, //!< Command given in wrong wallet encryption state (encrypting an encrypted wallet etc.)
     RPC_WALLET_ENCRYPTION_FAILED    = -16, //!< Failed to encrypt the wallet
     RPC_WALLET_ALREADY_UNLOCKED     = -17, //!< Wallet is already unlocked
+
+    //! EQB errors
+    RPC_WALLET_INVALID_ISSUER_NAME  = -18, //!< Invalid issuer name
 };
 
 UniValue JSONRPCRequestObj(const std::string& strMethod, const UniValue& params, const UniValue& id);
