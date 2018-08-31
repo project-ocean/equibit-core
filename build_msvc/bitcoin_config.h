a93c65c2b190e4c5c14a8e9f15381e9405ef970a
Reza Soltani <rsoltani@live.ca>
2018-08-31 19:32:53 -0400

    #80 renamed all VS library files. BTC version changed to 16.2

--- build_msvc/bitcoin_config.h
+++ build_msvc/bitcoin_config.h
@@ -14,10 +14,10 @@
 #define CLIENT_VERSION_MAJOR 1
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 17
+#define CLIENT_VERSION_MINOR 16
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 99
+#define CLIENT_VERSION_REVISION 2
 
 /* Copyright holder(s) before %s replacement */
 #define COPYRIGHT_HOLDERS "The %s developers"
@@ -346,7 +346,7 @@
 #define PACKAGE_NAME "Bitcoin Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Bitcoin Core 0.17.99"
+#define PACKAGE_STRING "Bitcoin Core 0.16.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bitcoin"
@@ -355,7 +355,7 @@
 #define PACKAGE_URL "https://bitcoincore.org/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.17.99"
+#define PACKAGE_VERSION "0.16.2"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
