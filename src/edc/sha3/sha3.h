b6ddaf51714a13515ccb617342c6297ee88a8365
Reza Soltani <rsoltani@live.ca>
2018-09-11 10:46:26 -0400

    issue #90 - add SHA3 wrapper class. Add files to libequibit_crypto VS project

---
+++ src/edc/sha3/sha3.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2018 Equibit Group AG
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+#ifndef BUILD_BTC
+
+#include "SimpleFIPS202.h"
+#include "serialize.h"
+#include "uint256.h"
+#include "version.h"
+#include <vector>
+
+class Sha3
+{
+private:
+    std::vector<char> m_serialization;
+
+    const int m_type;
+    const int m_version;
+
+public:
+    Sha3(int type, int version) : m_type(type), m_version(version)
+    {
+        m_serialization.reserve(128);
+    }
+
+    int GetType() const { return m_type; }
+    int GetVersion() const { return m_version; }
+
+    void write(const char* pch, size_t size)
+    {
+        while (size--)
+            m_serialization.push_back(*pch++);
+    }
+
+    uint256 GetHash()
+    {
+        uint256 hash;
+
+        SHA3_256((unsigned char*)&hash, (unsigned char*)&m_serialization[0], m_serialization.size());
+
+        return hash;
+    }
+
+    template <typename T>
+    Sha3& operator<<(const T& obj)
+    {
+        ::Serialize(*this, obj);
+        return (*this);
+    }
+
+    template <typename T>
+    static uint256 SerializeHash(const T& obj, int type = SER_GETHASH, int version = PROTOCOL_VERSION)
+    {
+        Sha3 sha3(type, version);
+
+        sha3 << obj;
+
+        return sha3.GetHash();
+    }
+};
+#endif // END_BUILD
