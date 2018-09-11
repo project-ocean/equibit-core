3e28e1a36ebea870567d5a31dcbc12ce84543e3f
Alex Fadeev <ealefad@gmail.com>
2018-09-11 13:12:39 -0400

    #73 Add test_runner.sh  for manual functional testing

--- eqbtest/functional/test_runner.sh
+++ eqbtest/functional/test_runner.sh
@@ -1,6 +1,4 @@
 # This script runs the python test_runner.py calling all the functional tests
 # in two OS threads
 
-python3 test_runner.py --extended --jobs=2
-
-
+python3 test_runner.py --extended --exclude feature_pruning,feature_dbcrash --jobs=4
