716784b447a6d60ed62ac6b38be257860708bafc
Alex Fadeev <ealefad@gmail.com>
2018-09-25 17:05:31 -0400

    #125 Update for FT framework and two functional tests, related to rebranding

--- eqbtest/functional/rpc_signmessage.py
+++ eqbtest/functional/rpc_signmessage.py
@@ -19,7 +19,9 @@ class SignMessagesTest(BitcoinTestFramework):
         self.log.info('test signing with priv_key')
         priv_key = 'cUeKHd5orzT3mz8P9pxyREHfsWtVfgsfDjiZZBcjUBAaGk1BTj7N'
         address = 'mpLQjfK79b7CCV4VMJWEWAj5Mpx8Up5zxB'
-        expected_signature = 'INbVnW4e6PeRmsv2Qgu8NuopvrVjkcxob+sX8OcZG0SALhWybUjzMLPdAsXI46YZGb0KQTRii+wWIQzRpG/U+S0='
+        # EQB_TODO remove comment with obsolete expected_signature, related to validation.cpp:strMessageMagic
+        # expected_signature = 'INbVnW4e6PeRmsv2Qgu8NuopvrVjkcxob+sX8OcZG0SALhWybUjzMLPdAsXI46YZGb0KQTRii+wWIQzRpG/U+S0='
+        expected_signature = 'ICGsXwKikjD8ngfiQgq3fsA8e5XoUSDhNLi8brGMoNArbA0WS03MQ80NhXcQO3g+s+3TkoyY2eqIHx3gVVHQc2U='
         signature = self.nodes[0].signmessagewithprivkey(priv_key, message)
         assert_equal(expected_signature, signature)
         assert(self.nodes[0].verifymessage(address, signature, message))
