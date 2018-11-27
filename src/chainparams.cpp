203396e0d900804ef37461320fa619b3934475e5
Reza Soltani <rsoltani@live.ca>
2018-11-27 12:30:29 -0500

    #151 - updated genesis text

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -68,7 +68,7 @@ static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     //const char* pszTimestamp = "The Globe And Mail 28/Jun/2018 Blockchain has the pontential to do amazing things, but it needs a reboot";
-    const char* pszTimestamp = "The G&M 28/Jun/2018 Blockchain has the pontential to do amazing things";
+    const char* pszTimestamp = "WSJ - 03/Jan/2018 - Billionaire Wagers Millions On Bitcoin";
     const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
