f2d61b4a97134b6fdb56c0c61e6667ddef6cadf9
Alex Fadeev <ealefad@gmail.com>
2018-11-29 20:36:37 -0500

    #231 FT fix related to Genesis timestamp

--- eqbtest/functional/test_framework/test_framework.py
+++ eqbtest/functional/test_framework/test_framework.py
@@ -331,7 +331,7 @@ class BitcoinTestFramework():
         For backwared compatibility of the python scripts with previous
         versions of the cache, this helper function sets mocktime to Jan 1,
         2014 + (201 * 10 * 60)"""
-        self.mocktime = 1388534400 + (201 * 10 * 60)
+        self.mocktime = 1543168979 + (201 * 10 * 60)
 
     def disable_mocktime(self):
         self.mocktime = 0
