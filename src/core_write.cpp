77f0b20520e60ef2d3aa4206398716493f89798e
David McFadzean <davidmc@gmail.com>
2018-12-17 18:24:32 -0500

    Reset nonce in testnet genesis block

--- src/core_write.cpp
+++ src/core_write.cpp
@@ -210,8 +210,8 @@ void TxToUniv(const CTransaction& tx, const uint256& hashBlock, UniValue& entry,
     entry.pushKV("vout", vout);
 
 #ifndef BUILD_BTC
-    entry.pushKV("EQB_type", (int8_t)tx.nEQBType);
-    entry.pushKV("EQB_payload", tx.eqbPayload.ToString());
+    //entry.pushKV("EQB_type", (int8_t)tx.nEQBType);
+    //entry.pushKV("EQB_payload", tx.eqbPayload.ToString());
 #endif // END_BUILD
 
     if (!hashBlock.IsNull())
