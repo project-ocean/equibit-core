e980e33f997c1588f1c174395321258f7e4fa279
David McFadzean <davidmc@gmail.com>
2019-01-03 11:38:19 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -16,7 +16,6 @@
 
 #include <chainparamsseeds.h>
 
-#include <pow.h>
 static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     CMutableTransaction txNew;
