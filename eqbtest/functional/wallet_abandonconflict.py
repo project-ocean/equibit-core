e7ab2a31b7317078ff9c869c3823fd51b81571a6
Alex Fadeev <ealefad@gmail.com>
2019-01-03 18:11:58 -0500

    #291 Clean up

--- eqbtest/functional/wallet_abandonconflict.py
+++ eqbtest/functional/wallet_abandonconflict.py
@@ -19,10 +19,7 @@ class AbandonConflictTest(BitcoinTestFramework):
         self.extra_args = [["-minrelaytxfee=0.00001"], []]
 
     def run_test(self):
-<<<<<<< HEAD
         #raise SkipTest("Disabled to make issues/#20-tx-structure pass")  # EQB_TODO: disabled test
-=======
->>>>>>> parent of fea8f47ab... Issues/#20 tx structure (#220)
         self.nodes[1].generate(100)
         sync_blocks(self.nodes)
         balance = self.nodes[0].getbalance()
