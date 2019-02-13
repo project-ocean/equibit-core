cf713b0ffdb76c7dc2df123bd4df63c80410097e
Alex Fadeev <ealefad@gmail.com>
2019-02-13 10:21:37 -0500

    #351 Update publish.sh to overwrite old binaries

--- publish.sh
+++ publish.sh
@@ -2,13 +2,13 @@
 # Upload Windows exe files to https://github.com/Equibit/CoreBinaries/dev/
 
 EQUIBIT_VERSION=0.2.1
-BUILD_TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+BUILD_TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
 echo "--------------------- Publishing Core Binaries ---------------------"
 git clone https://$GH_USR:$GH_TKN@github.com/Equibit/CoreBinaries.git
-cd CoreBinaries/dev && mkdir -p ${EQUIBIT_VERSION}/${BUILD_TIMESTAMP}/
-cp /home/travis/build/Equibit/equibit-core/build/equibit-x86_64-w64-mingw32/src/*.exe ${EQUIBIT_VERSION}/${BUILD_TIMESTAMP}
+cd CoreBinaries/dev && mkdir ${EQUIBIT_VERSION}
+cp -f /home/travis/build/Equibit/equibit-core/build/equibit-x86_64-w64-mingw32/src/*.exe ${EQUIBIT_VERSION}
 git add *
-git commit -m "Copy Equibit binaries to ${EQUIBIT_VERSION}/${BUILD_TIMESTAMP}"
+git commit -m "Equibit binaries updated on ${BUILD_TIMESTAMP}"
 git push origin master
 
