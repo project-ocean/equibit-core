08d795f1d18c1eccb55ea5d58dffc24f8dd90e60
Alex Fadeev <ealefad@gmail.com>
2019-02-12 21:41:02 -0500

    #348 Add publish.sh script and update .travis.yml to upload Windows binaries

--- publish.sh
+++ publish.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-# deploying win exe files to https://github.com/Equibit/CoreBinaries/dev/
+# Upload Windows exe files to https://github.com/Equibit/CoreBinaries/dev/
 
+EQUIBIT_VERSION=0.2.1
 BUILD_TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 echo "--------------------- Publishing Core Binaries ---------------------"
-echo $BUILD_TIMESTAMP
-
-# ls -laR /home/travis/build/Equibit/equibit-core/build/equibit-x86_64-w64-mingw32
-
 git clone https://$GH_USR:$GH_TKN@github.com/Equibit/CoreBinaries.git
-cd CoreBinaries/dev && mkdir -p 0.2.1/${BUILD_TIMESTAMP}/
-cp /home/travis/build/Equibit/equibit-core/build/equibit-x86_64-w64-mingw32/src/*.exe 0.2.1/${BUILD_TIMESTAMP}
+cd CoreBinaries/dev && mkdir -p ${EQUIBIT_VERSION}/${BUILD_TIMESTAMP}/
+cp /home/travis/build/Equibit/equibit-core/build/equibit-x86_64-w64-mingw32/src/*.exe ${EQUIBIT_VERSION}/${BUILD_TIMESTAMP}
 git add *
-git commit -m "Copy Equibit binaries to 0.2.1/${BUILD_TIMESTAMP}"
+git commit -m "Copy Equibit binaries to ${EQUIBIT_VERSION}/${BUILD_TIMESTAMP}"
 git push origin master
 
