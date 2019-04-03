e8b93bc5b005138168992298e557bd125ed848bd
David McFadzean <davidmc@gmail.com>
2019-04-03 11:23:11 -0400

    Updated binaries repo target

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ EQUIBIT_VERSION=0.2.1
 BUILD_TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
 echo "--------------------- Publishing Core Binaries ---------------------"
-git clone https://$GH_USR:$GH_TKN@github.com/Equibit/CoreBinaries.git
+git clone https://$GH_USR:$GH_TKN@github.com/project-ocean/windows-binaries.git
 cd CoreBinaries/dev && mkdir ${EQUIBIT_VERSION}
 cp -f /home/travis/build/Equibit/equibit-core/build/equibit-x86_64-w64-mingw32/src/*.exe ${EQUIBIT_VERSION}
 git add *
