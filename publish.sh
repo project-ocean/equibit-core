cf6064579b7b80e1cb85e0ac16447d605e391e42
David McFadzean <davidmc@gmail.com>
2019-04-03 17:02:37 -0400

    Fixed artifacts path

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ BUILD_TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 echo "--------------------- Publishing Core Binaries ---------------------"
 git clone https://$GH_USR:$GH_TKN@github.com/project-ocean/windows-binaries.git
 cd windows-binaries/node && mkdir ${EQUIBIT_VERSION}
-cp -f /home/travis/build/Equibit/equibit-core/build/equibit-x86_64-w64-mingw32/src/*.exe ${EQUIBIT_VERSION}
+cp -f /home/travis/build/project-ocean/equibit-core/build/equibit-x86_64-w64-mingw32/src/*.exe ${EQUIBIT_VERSION}
 git add *
 git commit -m "Equibit binaries updated on ${BUILD_TIMESTAMP}"
 git push origin master
