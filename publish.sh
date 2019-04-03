63cc5914d23f525672b60db3f68fc39b6b363ffa
David McFadzean <davidmc@gmail.com>
2019-04-03 17:01:15 -0400

    Fixed folder path

--- publish.sh
+++ publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Upload Windows exe files to https://github.com/Equibit/CoreBinaries/dev/
+# Upload Windows exe files to https://github.com/project-ocean/windows-binaries/node/
 
 EQUIBIT_VERSION=0.2.1
 BUILD_TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
 echo "--------------------- Publishing Core Binaries ---------------------"
 git clone https://$GH_USR:$GH_TKN@github.com/project-ocean/windows-binaries.git
-cd CoreBinaries/dev && mkdir ${EQUIBIT_VERSION}
+cd windows-binaries/node && mkdir ${EQUIBIT_VERSION}
 cp -f /home/travis/build/Equibit/equibit-core/build/equibit-x86_64-w64-mingw32/src/*.exe ${EQUIBIT_VERSION}
 git add *
 git commit -m "Equibit binaries updated on ${BUILD_TIMESTAMP}"
